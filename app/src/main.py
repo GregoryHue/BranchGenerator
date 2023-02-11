6a9ba5a72c4eb6a000a2550f2a770a1f208896e6
Gregory <gregory.hue135@gmail.com>
2023-02-11 18:45:55 +0100

    Trying display a branch

--- app/src/main.py
+++ app/src/main.py
@@ -4,9 +4,9 @@ import controller
 
 def main():
 
-    
     branch = controller.generate_branch()
-    view.display_branch(branch)
+    view.show_to_console(branch)
+
 
 if __name__ == '__main__':
     main()
--- app/src/view.py
+++ app/src/view.py
@@ -1,4 +1,30 @@
 
+def create_canvas():
+    canva_h = 32
+    canva_w = 16
 
-def display_branch(branch):
+    canvas = [[' ' for i in range(0, canva_w)] for j in range(0, canva_h)]
+
+    
+    return canvas, canva_w
+
+
+def show_to_console(branch):
     print(branch)
+    canvas, canva_w = create_canvas()
+    canvas = branch_on_vancas(canvas, branch, canva_w)
+
+
+def branch_on_vancas(canvas, branch, canva_w, space=32):
+    for x in branch:
+        print(return_int(x))
+    
+
+
+def return_int(point):
+
+    if isinstance(point, int):
+        return point
+    elif isinstance(point, list):
+        for x in point:
+            return_int(x)
